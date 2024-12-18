dcfd89b5c2cb61451a2eecd4d03f21a945b7f7c2
Jobel <jobel101@gmail.com>
2024-12-18 22:31:40 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import { app,server } from "./socket/socket.js";
 
 
 const PORT=process.env.PORT ||5000;
+const __dirname = path.resolve();
 
 dotenv.config(); //for getting port from env
 
@@ -24,6 +25,12 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res) =>
+{
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+
+});
 
 server.listen(PORT,() => {
     MongoDBconnect();
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -5,31 +5,31 @@ import MessageSkeleton from "../skeletons/MessageSkeleton";
 import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
-  const {message,loading} =useGetMessages();
-  useListenMessages();
-  const lastMessageRef =useRef();
-   useEffect(() => {
-    lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+	const { messages, loading } = useGetMessages();
+	useListenMessages();
+	const lastMessageRef = useRef();
 
-   },[])
-  
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-      {!loading &&
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
-					<div key ={message._id}
-          ref={lastMessageRef}
-          >
-            <Message message={message} />
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
 					</div>
 				))}
-     {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-     {!loading && messages.length === 0 && (
+
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
 				<p className='text-center'>Send a message to start the conversation</p>
 			)}
-    </div>
-  )
-}
-
-export default Messages
+		</div>
+	);
+};
+export default Messages;
