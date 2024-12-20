b9db88d2083668b1635bb87493bfa94b7fd712fe
Jobel Iype <72402429+jobel1@users.noreply.github.com>
2024-12-20 14:54:35 +0530

    tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,7 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("daisyui"),
-  ],
+  plugins: ["daisyui"],
 }
